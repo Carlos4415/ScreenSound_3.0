0f530420a04547d70d1650e006cf15b54b9f9bd5
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-19 19:01:58 -0300

    first commit

---
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,44 @@
+﻿using OllamaSharp;
+using OllamaSharp.Models;
+using ScreenSound.Modelos;
+using System.Text;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var ollama = new OllamaApiClient("http://localhost:11434");
+
+        var request = new GenerateRequest
+        {
+            Model = "llama3.2:latest",
+            Prompt = $"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal."
+        };
+
+        var resposta = new StringBuilder();
+
+        Task.Run(async () =>
+        {
+            await foreach (var stream in ollama.GenerateAsync(request))
+            {
+                resposta.Append(stream.Response);
+            }
+        }).GetAwaiter().GetResult();
+
+        banda.Resumo = resposta.ToString();
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
---
+++ ScreenSound/README.md
@@ -0,0 +1,10 @@
+# Programa Screen Sound 3.0
+
+Um sistema para registrar bandas, álbuns e avaliações musicais.
+
+Permite ao usuário adicionar bandas, registrar álbuns, exibir a lista de bandas, avaliar bandas e visualizar detalhes, incluindo a média das avaliações.
+
+## Clonando o repositório
+
+```bash
+git clone https://github.com/Carlos4415/ScreenSound_3.0.git
